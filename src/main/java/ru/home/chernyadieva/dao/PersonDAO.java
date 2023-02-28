33f810bd4a4471e3158899b50cedcb50faf62441
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-03-01 02:16:53 +0400

    spring-CRUD-app
    добавлен функционал по автоинкрементному обновлению поля id в БД

--- src/main/java/ru/home/chernyadieva/dao/PersonDAO.java
+++ src/main/java/ru/home/chernyadieva/dao/PersonDAO.java
@@ -1,6 +1,5 @@
 package ru.home.chernyadieva.dao;
 
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -35,7 +34,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(1,?,?,?)",
+        jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?,?,?)",
                 person.getName(),
                 person.getAge(),
                 person.getEmail());
