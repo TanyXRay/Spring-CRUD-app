7613e90c277f6761e1b784b0f0d2265a4dcaead9
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-03-09 16:36:54 +0400

    spring-CRUD-app
    создана ветка hibernate, обновлены данные в классе SpringConfig

--- src/main/java/ru/home/chernyadieva/config/SpringConfig.java
+++ src/main/java/ru/home/chernyadieva/config/SpringConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,27 +16,35 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Objects;
+
 /**
  * Класс конфигурация вместо файла applicationContextMVC.xml
  */
 @Configuration
 @ComponentScan("ru.home.chernyadieva") //ищет контроллеры в указанной директории
 @EnableWebMvc
+@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
+    private final Environment environment;
 
     /**
-     * Внедряем ApplicationContext
+     * Внедряем ApplicationContext, Environment
+     *
      * @param applicationContext
      */
     @Autowired
-    public SpringConfig(ApplicationContext applicationContext) {
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
         this.applicationContext = applicationContext;
+        this.environment = environment;
     }
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
         var templateResolver = new SpringResourceTemplateResolver();
+
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/view/");
         templateResolver.setSuffix(".html");
@@ -43,6 +55,7 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
     public SpringTemplateEngine templateEngine() {
         var templateEngine = new SpringTemplateEngine();
+
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
@@ -50,13 +63,36 @@ public class SpringConfig implements WebMvcConfigurer {
 
     /**
      * Здесь задаем шаблонизатор, в данном случае Thymeleaf
+     *
      * @param registry
      */
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         var resolver = new ThymeleafViewResolver();
+
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
         resolver.setCharacterEncoding("UTF-8");
+    }
+
+    /**
+     * Метод передачи данных сервера jdbc
+     *
+     * @return
+     */
+    @Bean
+    public DataSource dataSource() {
+        var dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName((Objects.requireNonNull(environment.getProperty("driver"))));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("user_name"));
+        dataSource.setPassword(environment.getProperty("password"));
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
